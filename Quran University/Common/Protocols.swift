6e1ecf59af15537c2e9662925879b7e360e78c31
mac <mac@macs-macbook-pro.local>
2018-04-04 17:37:53 +0500

    fix.

--- Quran University/Common/Protocols.swift
+++ Quran University/Common/Protocols.swift
@@ -6,7 +6,3 @@ import Foundation
     @objc optional func onDoneHandler(StartSurahId: Int64, EndSurahId: Int64, StartAyatOrderId: Int64, EndAyatOrderId: Int64, AyatRecitationSilence: Double, AyatRepeatFor: Int64, RangeRecitationSilence: Double, RangeRepeatFor: Int64)
     @objc optional func onCancelHandler()
 }
-
-protocol ExpandableHeaderViewDelegate {
-    func toggleSection(header: AssignmentHeaderFooterView, section: Int)
-}
